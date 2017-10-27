95129fe4a8c3bfd3c596f0edc313d8ff722e1c91
JeeOpp <Demkoandrey2012@gmal.com>
2017-10-27 16:08:40 +0300

    Jee

--- task02/src/main/java/Main.java
+++ task02/src/main/java/Main.java
@@ -8,7 +8,7 @@ import java.util.List;
 public class Main {
 
     public static void main(String[] args) {
-        final String path = "locator.xml";
+        final String path = "pomtest.xml";
 
         ServiceFactory serviceFactory = ServiceFactory.getInstance();
         ParserService parserService;
